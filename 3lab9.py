0fea05bf55b5cd5fb93ced54433245c35ee4c151
VictoriaOshchepkova <viktoshepkova@gmail.com>
2024-11-18 15:17:27 +0500

    Update 3lab9.py

--- 3lab9.py
+++ 3lab9.py
@@ -1,20 +1,22 @@
 import numpy as np
 
-a = np.array([float(x) for x in input("Введите координаты вектора a: ").split()])
-b = np.array([float(x) for x in input("Введите координаты вектора b: ").split()])
-c = np.array([float(x) for x in input("Введите координаты вектора c: ").split()])
+a = [float(x) for x in input("Введите координаты вектора a: ").split()]
+b = [float(x) for x in input("Введите координаты вектора b: ").split()]
+c = [float(x) for x in input("Введите координаты вектора c: ").split()]
 while True:
     if len(a) == 3 and len(b) == 3 and len(c) == 3:
         break
     else:
         print('Размерность векторов равна трем!')
         if len(a) != 3:
-            a = np.array([float(x) for x in input("Введите координаты вектора a: ").split()])
+            a = [float(x) for x in input("Введите координаты вектора a: ").split()]
         if len(b) != 3:
-            b = np.array([float(x) for x in input("Введите координаты вектора b: ").split()])
+            b = [float(x) for x in input("Введите координаты вектора b: ").split()]
         if len(c) != 3:
-            c = np.array([float(x) for x in input("Введите координаты вектора c: ").split()])
-    
+            c = [float(x) for x in input("Введите координаты вектора c: ").split()]
+a = np.array(a)
+b = np.array(b)
+c = np.array(c)
     
 d = 2 * c + (a - b) + (b - c)
 
