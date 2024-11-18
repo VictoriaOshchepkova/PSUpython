b2f217935a4603681d0f087d6c1a36209313f738
VictoriaOshchepkova <viktoshepkova@gmail.com>
2024-11-18 14:29:03 +0500

    Create 1lab9.py

---
+++ 1lab9.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+while True:
+    n = int(input('Введите нечетное натуральное число: '))
+    if n % 2 == 0:
+        print('Введите корректное значение!')
+    if n % 2 != 0:
+        break
+    
+m = np.full((n,n),'.')
+
+m[1::2] = '*'
+m[::, 1::2] = '*'
+        
+print('Вы получили массив:')
+for row in m:
+    print(' '.join(row))
